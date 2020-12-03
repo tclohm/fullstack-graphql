8a7f26926dbd300b0c7ae402b63617049840175b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-12-03 09:24:45 -0800

    added apollo-client-devtool

--- client/src/client.js
+++ client/src/client.js
@@ -3,6 +3,30 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import gql from 'graphql-tag'
 
-/**
- * Create a new apollo client and export as default
- */
+const link = new HttpLink({uri: 'https://rickandmortyapi.com/graphql'});
+
+// MARK: cache
+const cache = new InMemoryCache();
+
+
+// MARK: -- simple way to create apollo client
+const client = new ApolloClient({
+	link,
+	cache
+});
+
+const query = gql`
+	{
+		characters {
+			results {
+				id
+				name
+			}
+		}	
+	}
+`;
+
+client.query({query})
+	  .then(result => console.log(result))
+
+export default client;
