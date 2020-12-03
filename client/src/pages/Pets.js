be3c7a8b09c289e713b66843f1ccb81a628e82dc
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-12-03 09:35:37 -0800

    provider wrappred around app passing client

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,9 +5,12 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const query = gql``;
+
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
+  const [] = useQuery()
 
   const onSubmit = input => {
     setModal(false)
