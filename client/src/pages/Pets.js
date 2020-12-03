c8509eabfd7f35541329d23cc7deebbff6bed75e
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-12-03 13:56:26 -0800

    added pets to db.json and placed query and mutation

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,19 +5,48 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
-const query = gql``;
+const GET_PETS = gql`
+  query petsList {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
+const CREATE_PET = gql`
+  mutation AddPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      img
+      type
+    }
+  }
+`;
 
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
 
+  const {data, loading, error} = useQuery(GET_PETS);
+  const [addPet, newPet] = useMutation(CREATE_PET);
+
   const onSubmit = input => {
     setModal(false)
+    console.log(input);
+    addPet({
+      variables: {newPet: input}
+    })
   }
   
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
+  if (loading || newPet.loading) return <Loader />
+  if (error || newPet.error) return <p>ERROR</p>
 
   return (
     <div className="page pets-page">
@@ -33,7 +62,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
