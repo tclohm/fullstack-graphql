cfd4be91aded08856394de6b5705f37946db2f7c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-12-03 17:02:22 -0800

    optimistic response and caching

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -32,11 +32,28 @@ export default function Pets () {
   const [modal, setModal] = useState(false)
 
   const {data, loading, error} = useQuery(GET_PETS);
-  const [addPet, newPet] = useMutation(CREATE_PET);
+  const [addPet, newPet] = useMutation(CREATE_PET, {
+    update(cache, { data: {addPet} }) {
+      const { pets } = cache.readQuery({ query: GET_PETS })
+      cache.writeQuery({
+        query: GET_PETS,
+        data: { pets: [addPet, ...pets] }
+      })
+    },
+    optimisticResponse: {
+      __typename: "Mutation",
+      addPet: {
+        id: Math.floor(Math.random() * 100000) + "",
+        name: input.name,
+        type: input.type,
+        img: "ok",
+        __typename: "Pet",
+      }
+    }
+  });
 
   const onSubmit = input => {
     setModal(false)
-    console.log(input);
     addPet({
       variables: {newPet: input}
     })
